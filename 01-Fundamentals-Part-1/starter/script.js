7badc362823b5d58aa95731e202a8185380745ee
kevinaoliver <kevin.oliver021@gmail.com>
2023-09-09 17:00:18 -0500

    Added course materials and fundamentals pt 1 script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Bob";
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof true);
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof "Kevin");
+*/
+
+let country = "USA";
+let continent = "North America";
+let population = "330000000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
